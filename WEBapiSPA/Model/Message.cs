4bee85a518628f72a3bf47066737f088e9583d21
К Б <yabkn@outlook.com>
2025-01-15 13:20:13 +0700

    Add project files.

---
+++ WEBapiSPA/Model/Message.cs
@@ -0,0 +1,12 @@
+﻿namespace WEBapiSPA.Model
+{
+    public class Message
+    {
+        public Guid Id { get; set; }
+        public Guid Device { get; set; }
+        public string UserName { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public string VersionPA { get; set; }
+    }
+}
---
+++ WEBapiSPA/Program.cs
@@ -0,0 +1,26 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Configure the HTTP request pipeline.
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+//app.UseAuthorization();
+//app.UseHttpsRedirection();
+
+app.MapControllers();
+
+app.Run();
